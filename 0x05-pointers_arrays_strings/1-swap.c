be63a7fdac75818218053a1df41c2ddb8737c4ff
Samuel Osunsakin <samstarrysam@gmail.com>
2023-07-12 11:32:22 +0000

    Wrote a function that swaps integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * swap_int - Function that swaps the paremeters
+ * @a: First parameter
+ * @b: Second parameter
+ *
+ * Return: Void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int s;
+
+	*a = s;
+	*a = *b;
+	*b = s;
+}
