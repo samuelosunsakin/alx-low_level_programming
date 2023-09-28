b2c868946a461cbce618abed3186cf090deb909a
Samuel Osunsakin <samstarrysam@gmail.com>
2023-09-28 20:32:36 +0000

    Wrote a recursive power function

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Funtion that that acts like the pow function
+ * @x: Base parameter
+ * @y: Degree parameter
+ *
+ * Return: integer answer
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else
+		return(x * _pow_recursion(x, --y);
+}
