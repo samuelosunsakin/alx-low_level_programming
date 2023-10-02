a030f678f0a01c036ccef23f4f185257da24919d
Samuel Osunsakin <samstarrysam@gmail.com>
2023-10-02 19:38:58 +0000

    Wrote a program that prints the number of argugents

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,26 +1,23 @@
 #include <stdio.h>
 
 /**
- * main - Program that prints number of arguments
+ * main - Program that prints the number of arguments
  * @argc: Argument count
  * @argv: Argument vector
+ *
  * Return: 0 on success
  */
 
-int main(int argc, char **argv)
+int main(int argc, __attribute__ ((unused)) char **argv)
 {
-	int i;
+	int i = 0;
 
-	if (argc == 1)
-		printf("%d\n", argc - 1);
-	else
+	if (argc > 1)
 	{
-		for (i = 0; *argv; i++, argv++)
-		{
-
-		}
-		printf("%d\n", i - 1);
+		printf("%d\n", argc - 1);
 	}
+	else
+		printf("%d\n", i);
 
 	return (0);
 }
